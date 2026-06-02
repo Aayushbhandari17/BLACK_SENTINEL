76da68b54139fc5450729961652e1709418ae7a8
Aayush Bhandari <aayushbhandari@Aayushs-Laptop-3.local>
2026-06-02 15:31:12 +0530

    Sync: remove deleted files

--- test_entropy.py
+++
@@ -1,9 +0,0 @@
-from black_sentinel.detection.entropy_engine import scan
-
-with open("/Users/aayushbhandari/Desktop/test_entropy.txt", "r") as f:
-    content = f.read()
-
-results = scan(content)
-
-for r in results:
-    print(r)
